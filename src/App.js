0719a92c98ea34329bcce3ecac850835edd1d72b
AustinT12 <austintran85@gmail.com>
2022-08-27 13:43:43 -0400

    adding firebase imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'fire/app';
+import 'firebase/firestore';
+import 'firebase/auth/auth.js';
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.intializeApp({
+  apiKey: "AIzaSyAFNCjP4S6XgcgJdtm_FuDMQFk-v-_zbMI",
+  authDomain: "chat-messenger-dd901.firebaseapp.com",
+  projectId: "chat-messenger-dd901",
+  storageBucket: "chat-messenger-dd901.appspot.com",
+  messagingSenderId: "105290050750",
+  appId: "1:105290050750:web:6b3a514e45bf76600f82ef",
+  measurementId: "G-Y40XQE5J3Y"
+})
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
